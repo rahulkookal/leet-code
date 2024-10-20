17069bd7d9e2165adc3e4760314afd3726b4b533
rahulkookal <rahulkkodoth@gmail.com>
2024-10-20 12:51:29 +0530

    code clean

--- 1493. Longest Subarray of 1's After Deleting One Element/solution.js
+++ 1493. Longest Subarray of 1's After Deleting One Element/solution.js
@@ -3,22 +3,30 @@
  * @return {number}
  */
 var longestSubarray = function (nums) {
-    let curr = 0;
-    if (nums.length == 0) {
-        return curr
+    let result = 0;
+    
+    if (nums.length === 0) {
+        return result;
     }
+
     let left = -1, right = 0;
+    
     for (let i = -1; i < nums.length;) {
-        while(nums[i+right+1] != 0 && (i+right+1) <nums.length){
-            right++
+        // Move `right` forward while the element is not zero and within bounds
+        while (nums[i + right + 1] !== 0 && (i + right + 1) < nums.length) {
+            right++;
         }
         
-        if(curr < left + right){
-            curr = left + right;
+        // Update the longest subarray length
+        if (result < left + right) {
+            result = left + right;
         }
-        i = i+right+1;
+
+        // Move to the next segment
+        i = i + right + 1;
         left = right;
         right = 0;
     }
-    return curr
+    
+    return result;
 };
