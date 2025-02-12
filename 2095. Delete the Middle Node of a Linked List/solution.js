8fe68f6b793c072f50bf4e17ab8f7305ef7e5e37
rahulkookal <rahulkkodoth@gmail.com>
2025-02-12 15:18:53 +0530

    2095. Delete the Middle Node of a Linked List

---
+++ 2095. Delete the Middle Node of a Linked List/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+    if(head== null) return null;
+    const prev = new ListNode(0);
+    prev.next = head
+    let slow = prev;
+    let fast = head
+    while(fast != null && fast.next!=null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    slow.next = slow.next.next
+    return prev.next
+};
