a52c3f361c1bb8ee7bf954317eb94717d535ae25
rahulkookal <rahulkkodoth@gmail.com>
2025-02-13 09:45:35 +0530

    238. Product of Array Except Self

---
+++ 238. Product of Array Except Self/solution.go
@@ -0,0 +1,19 @@
+func productExceptSelf(nums []int) []int {
+	length := len(nums)
+	result := make([]int, length)
+
+	// Compute left products
+	result[0] = 1
+	for i := 1; i < length; i++ {
+		result[i] = result[i-1] * nums[i-1]
+	}
+
+	// Compute right products and update result
+	R := 1
+	for i := length - 1; i >= 0; i-- {
+		result[i] *= R
+		R *= nums[i]
+	}
+
+	return result
+}
