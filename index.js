a20d19d7e8bf5c81d2cd1e8ef82a2c2a4add203c
rahulkookal <rahulkkodoth@gmail.com>
2025-03-18 17:52:35 +0000

    new problem

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("helo world")
+
+
+async function abc() {
+    setTimeout(() => console.log("Timeout fired"), 1000);
+   
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("Await completed");
+   
+    setImmediate(() => console.log("Immediate fired"));
+  }
